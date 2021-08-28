95e6c90653b2fd7e0fe4705b41fc9392e3429a0c
lwq <1594755774@qq.com>
2021-08-29 02:58:26 +0800

    hhh

--- vite.config.js
+++ vite.config.js
@@ -20,8 +20,8 @@ module.exports = {
    * @default '/'
    */
   base: "/lwq/",
-  publicPath: "./",
-  assetsDir: "./",
+  // publicPath: "./",
+  // assetsDir: "./",
   /**
    * 与“根”相关的目录，构建输出将放在其中。如果目录存在，它将在构建之前被删除。
    * @default 'dist'
