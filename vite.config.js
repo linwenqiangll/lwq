021c3cade781f2932f48e9034cfc5d4ec7ccfef0
“ylwq <y1594755774@qq.com>
2021-08-28 17:59:31 +0800

    lwq

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@
  * @Autor: lwq
  * @Date: 2021-08-28 17:18:26
  * @LastEditors: lwq
- * @LastEditTime: 2021-08-28 17:18:27
+ * @LastEditTime: 2021-08-28 17:59:15
  * @FilePath: \first-vue3\vite.config.js
  */
 module.exports = {
-  publicPath: '/jjj/',
+  publicPath: '/lwq/',
 };
