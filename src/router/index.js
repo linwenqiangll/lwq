7bfba19953bc4903fdb2a2f51cf6e90237fb48b0
lwq <1594755774@qq.com>
2021-08-29 18:21:59 +0800

    3

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import TODO from "../views/todo.vue";
 
 const router = createRouter({
   routes: [{ path: "/todo", component: TODO }],
-  history: createWebHashHistory(),
+  history: createWebHistory("hahahha"),
 });
 export default router;
